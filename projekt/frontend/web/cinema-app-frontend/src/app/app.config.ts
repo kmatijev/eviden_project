e8ec3065b9689a05477abb404b1d7f68e6728ee1
iberlancic02 <ibro.ivan352@gmail.com>
2024-07-02 09:20:04 +0200

    Fixed conflicts

--- projekt/frontend/web/cinema-app-frontend/src/app/app.config.ts
+++ projekt/frontend/web/cinema-app-frontend/src/app/app.config.ts
@@ -3,14 +3,8 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideClientHydration } from '@angular/platform-browser';
-<<<<<<< HEAD
-
-export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes), provideClientHydration()]
-=======
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideRouter(routes), provideClientHydration(), provideAnimationsAsync()]
->>>>>>> dspajic/homepage
 };
