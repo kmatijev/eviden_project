985b59a85f74463c36fc3ee24922ac31a272af41
Matej Mijok <matej.mijok@gmail.com>
2024-07-05 11:18:07 +0200

    implement seat grid view from data grabbed from an api

---
+++ projekt/frontend/android/Cinematicketreservation/app/src/main/java/hr/atos/praksa/cinematicketreservation/view/adapters/SeatAdapter.kt
@@ -0,0 +1,4 @@
+package hr.atos.praksa.cinematicketreservation.view.adapters
+
+class SeatAdapter {
+}
---
+++ projekt/frontend/android/Cinematicketreservation/app/src/main/java/hr/atos/praksa/cinematicketreservation/view/ui/SeatsFragment.kt
@@ -0,0 +1,4 @@
+package hr.atos.praksa.cinematicketreservation.view.ui
+
+class SeatsFragment {
+}
